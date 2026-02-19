97482e0a2efd08e0a0c30d948a7f5f8629c91b95
nells827 <nellyracky21@gmail.com>
2026-02-19 08:03:34 -0800

    test files

---
+++ course.java
@@ -0,0 +1,22 @@
+// File: Course.java
+/**
+ * Simple Course class to show composition
+ * (Student HAS-A Course)
+ */
+public class Course {
+    private String courseName;
+    private String courseCode;
+
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+}
